a569c40d30ba4dacbf35c36d02a8b6dbe8e27f17
Anne-Sophie Saint-Omer <anne.sophie.saint.omer28@gmail.com>
2015-11-16 23:44:55 +0100

    Correction dans les dates et ajout du clic long

--- app/src/main/java/com/anne_sophie/watteringday/ListPlantsActivity.java
+++ app/src/main/java/com/anne_sophie/watteringday/ListPlantsActivity.java
@@ -8,8 +8,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -29,7 +29,6 @@ public class ListPlantsActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_plants);
-
         bd = new PlantsDatabase(this);
     }
 
@@ -39,8 +38,8 @@ public class ListPlantsActivity extends ListActivity {
 
         // on récupère la liste des Personnes dans la BDD, sous forme de ArrayList
         plants = bd.getPlants();
+
         // on affiche la liste dans une ListView
-        //setListAdapter(new ArrayAdapter<Plant>(this, android.R.layout.simple_list_item_1, plants));
         setListAdapter(new NameAndDaysAdapter(this));
     }
 
@@ -68,13 +67,14 @@ public class ListPlantsActivity extends ListActivity {
 
     class NameAndDaysAdapter extends ArrayAdapter<Plant> {
 
+
         NameAndDaysAdapter(Context context) {
             super(context, R.layout.linesoflist, plants);
         }
 
         protected int compareNumberOfDays(Timestamp numberOfDays1, Timestamp numberOfDays2) {
             long nbMilliSec = numberOfDays1.getTime() - numberOfDays2.getTime();
-            double numberOfDays = ((double)nbMilliSec) / (60 * 60 * 24);
+            double numberOfDays = ((double)nbMilliSec) / (1000 * 60 * 60 * 24);
             return (int) (Math.round(numberOfDays));
         }
 
@@ -86,38 +86,61 @@ public class ListPlantsActivity extends ListActivity {
                 line = xml.inflate(R.layout.linesoflist, null);
             }
 
+            list = getListView();
+
             TextView namePlant = (TextView) line.findViewById(R.id.namePlant);
             TextView daysPlant = (TextView) line.findViewById(R.id.daysPlant);
 
 
-                Plant p = plants.get(position);
+            list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 
-                int numberOfDays = p.getWatteringPlant();
-
-                Long lastday_long = p.getWatteringDay();
-                Timestamp lastday = new Timestamp(lastday_long);
-
-                Long today_long = System.currentTimeMillis();
-                Timestamp today = new Timestamp(today_long);
-
-                String nameAndWatteringPlants = getListAdapter().getItem(position).toString();
-                String[] data = nameAndWatteringPlants.split(" ");
-
-                namePlant.setText(data[4]);
-                daysPlant.setText(data[5]);
-
-                if (this.compareNumberOfDays(today, lastday) == numberOfDays || this.compareNumberOfDays(today, lastday) == numberOfDays - 1) {
-                    line.setBackgroundColor(Color.rgb(237, 127, 16));
-                }
-                else if (this.compareNumberOfDays(today, lastday) < numberOfDays) {
+                @Override
+                public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                    View line = view;
+                    if (line == null) {
+                        LayoutInflater xml = ListPlantsActivity.this.getLayoutInflater();
+                        line = xml.inflate(R.layout.linesoflist, null);
+                    }
                     line.setBackgroundColor(Color.GREEN);
+                    Plant p = plants.get(position);
+                    p.setWatteringDay(System.currentTimeMillis());
+                    ListPlantsActivity.this.bd.updatePlant(p);
+                    return true;
                 }
-                else if (this.compareNumberOfDays(today, lastday) > numberOfDays) {
-                    line.setBackgroundColor(Color.RED);
-                }
-                else {
-                    line.setBackgroundColor(Color.BLACK);
-                }
+            });
+
+
+            Plant p = plants.get(position);
+
+            int numberOfDays = p.getWatteringPlant();
+
+            Long lastday_long = p.getWatteringDay();
+            Timestamp lastday = new Timestamp(lastday_long);
+
+            Long today_long = System.currentTimeMillis();
+            Timestamp today = new Timestamp(today_long);
+
+            String nameAndWatteringPlants = getListAdapter().getItem(position).toString();
+            String[] data = nameAndWatteringPlants.split(" ");
+
+            namePlant.setText(data[4]);
+            daysPlant.setText(data[5]);
+
+            if (this.compareNumberOfDays(today, lastday) == 0) {
+                line.setBackgroundColor(Color.GREEN);
+            }
+            else if (this.compareNumberOfDays(today, lastday) == numberOfDays || this.compareNumberOfDays(today, lastday) == numberOfDays - 1) {
+                line.setBackgroundColor(Color.rgb(237, 127, 16));
+            }
+            else if (this.compareNumberOfDays(today, lastday) < numberOfDays) {
+                line.setBackgroundColor(Color.GREEN);
+            }
+            else if (this.compareNumberOfDays(today, lastday) > numberOfDays) {
+                line.setBackgroundColor(Color.RED);
+            }
+            else {
+                line.setBackgroundColor(Color.BLACK);
+            }
 
             return line;
         }
